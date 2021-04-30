67c8abfdbc78b48622c125070d25f333a2318ebc
khushxxl <khushaal.choithramani@gmail.com>
2021-04-30 09:52:13 +0530

    commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:fluttercatalog/home_page.dart';
+import 'package:fluttercatalog/pages/home_page.dart';
+import 'package:fluttercatalog/pages/login_page.dart';
+import 'package:fluttercatalog/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp
- extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-      
-      
-      
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        darkTheme: ThemeData(brightness: Brightness.dark),
+        initialRoute: "/",
+        routes: {
+          "/": (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => HomePage(),
+          MyRoutes.loginRoute: (context) => LoginPage()
+        });
   }
 }
